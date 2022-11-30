51a4ab2506dec393d5a26eef542a0fb82cc53877
pagedhack <samu1@outlook.es>
2022-11-29 20:55:59 -0600

    first commit

---
+++ Heroku/src/main/java/mx/uv/App.java
@@ -0,0 +1,24 @@
+package mx.uv;
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        port(getHerokuAssignedPort());
+        get("/hello", (req, res) -> "Hello Heroku World");
+    }
+
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567; //return default port if heroku-port isn't set (i.e. on localhost)
+    }
+}
